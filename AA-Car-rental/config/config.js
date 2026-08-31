5c2e167cc63bba853cd84145110e4e3abfe8ec56
RobelGM <112305671+roba23@users.noreply.github.com>
2026-08-31 13:01:22 +0300

    Update config.js

--- AA-Car-rental/config/config.js
+++ AA-Car-rental/config/config.js
@@ -6,7 +6,7 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 >>>>>>> e5d9f9a (i added a new order controller file to you)
 import mongoose from "mongoose";
 import 'dotenv/config';
-const url = "mongodb+srv://eyoelsinore7_db_user:LYXpggvTqVdXFPBk@cluster0.tg0l2bi.mongodb.net/?appName=Cluster0";
+const url = process.env.MONGO_URL;
 
 
 //const url = `mongodb://127.0.0.1:27017/${process.env.dbName}`;
